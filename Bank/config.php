3d7dd4a7e4f4da747a7861b06b4f0f70f6a33361
pranav-ahire <90307998+pranav-ahire@users.noreply.github.com>
2021-09-12 10:07:12 +0530

    Add files via upload

---
+++ Bank/config.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+	$servername = "localhost";
+	$username = "id17543855_bankproject";
+	$password = "Pr@nav3062001";
+	$dbname = "id17543855_bank";
+
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
